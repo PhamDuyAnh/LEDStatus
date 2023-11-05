ba9b7c36e2b2f6e3254d3ef990d91932e2097c91
CKD <phamduyanh@gmail.com>
2023-11-05 18:57:28 +0700

    Add files via upload

---
+++ Arduino_Library.ino
@@ -0,0 +1,19 @@
+#include "src/LEDStatus.h"
+#define LED_PIN 13
+
+LEDStatus ledstatus(LED_PIN, 1, 5, 2500, 1);
+
+void setup()
+{
+  Serial.begin(115200);
+}
+
+void loop()
+{
+  if(Serial.available() > 0)
+  {
+    ledstatus.set(Serial.parseInt());
+  }
+
+  ledstatus.run();
+}
